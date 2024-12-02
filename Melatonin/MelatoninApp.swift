4579a0bf30fbceef8a7615acfaffd67f9e27c24e
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-12-02 12:15:58 +0100

    Initial Commit

---
+++ Melatonin/MelatoninApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelatoninApp.swift
+//  Melatonin
+//
+//  Created by Felix Pultar on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MelatoninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
